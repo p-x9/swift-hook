2df5a4c25c19f8da5e9cde72b8c4b9b13ee08594
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-02-10 01:51:38 +0900

    bump `MachOKit` version to 0.11.1

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/p-x9/fishhook", branch: "spm"),
         .package(url: "https://github.com/p-x9/Echo", branch: "swift5.9"),
-        .package(url: "https://github.com/p-x9/MachOKit-SPM", .upToNextMajor(from: "0.11.0"))
+        .package(url: "https://github.com/p-x9/MachOKit-SPM", .upToNextMajor(from: "0.11.1"))
     ],
     targets: [
         .target(
